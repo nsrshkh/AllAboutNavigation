ea061345e6851d08032651aae454249ef12b9c3e
Nasir Shaikh <58065616+nsrshkh@users.noreply.github.com>
2022-03-13 17:21:14 +0100

    Initial commit

---
+++ app/src/main/java/com/example/allaboutnavigation/GameWonFragment.kt
@@ -0,0 +1,20 @@
+package com.example.allaboutnavigation
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import com.example.allaboutnavigation.databinding.FragmentGameWonBinding
+
+
+class GameWonFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        val binding: FragmentGameWonBinding = DataBindingUtil.inflate(
+            inflater, R.layout.fragment_game_won, container, false)
+        return binding.root
+    }
+}
---
+++ app/src/main/java/com/example/allaboutnavigation/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.allaboutnavigation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.allaboutnavigation.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+    }
+}
